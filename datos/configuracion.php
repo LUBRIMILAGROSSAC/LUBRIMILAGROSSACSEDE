2e58bc7a6e867a50468cbb10abfae353bc08bf97
LUBRIMILAGROSSAC <62679285+LUBRIMILAGROSSAC@users.noreply.github.com>
2020-07-20 15:47:15 -0500

    Add files via upload

---
+++ datos/configuracion.php
@@ -0,0 +1,22 @@
+
+<?php
+
+// Notificar todos los errores excepto E_NOTICE
+error_reporting(E_ALL ^ E_NOTICE);
+
+
+define("SERVIDOR_BD", "ec2-34-197-212-240.compute-1.amazonaws.com");
+define("USUARIO_BD", "xdneskcbzfnstx");
+define("CLAVE_BD", "7030939d3c33d28a863206b81ba27a33c0264585e474bb468abe6c1799469b4a");
+define("PUERTO_BD", "5432");
+define("NOMBRE_BD", "d5mgunngj64gdg");
+
+/*
+define("SERVIDOR_BD", "localhost");
+define("USUARIO_BD", "postgres");
+define("CLAVE_BD", "47767670");
+define("PUERTO_BD", "5432");
+define("NOMBRE_BD", "LUBRICENTRO");
+ */
+
+
